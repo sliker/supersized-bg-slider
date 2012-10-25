fac3dda8d38d157a5824fe5b4fbb533add044c97
David Velásquez <zero.blend@gmail.com>
2012-10-25 10:26:17 -0500

    added .php file

---
+++ supersized-bg-slider.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Supersized Bg Slider
+Plugin URI: https://github.com/sliker/supersized-bg-slider
+Description: Supersized Jquery plugin integrated to Wordpress
+Version: 0.1
+Author: David Velásquez
+Author URI: http://www.davidvu.co
+
+License: GPL2 - http://www.gnu.org/licenses/gpl.txt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+?>
